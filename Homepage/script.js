5e7dc559a626f711ecb4a77647b760946f1a0046
Vincent <vincentl105@nycstudents.net>
2024-01-17 10:50:25 -0500

    asdf

--- Homepage/script.js
+++ Homepage/script.js
@@ -18,18 +18,17 @@ function roll() {
     calculate();
 }
 
-function updateTable(calculations){
-    let tableRef = document.getElementById('data');
-    let newRow = tableRef.insertRow(-1);
-    for (i = 0; i < calculations.length; i ++){
-        let newCell = newRow.insertCell(i);
-        newCell.appendChild(document.createTextNode(calculations[i]));
-    }
-}
-
 function calculate(){
     let calculations = [mean(), median(), mode(), frequency(), freq2, freq3];
     updateTable(calculations);
+}
+
+function updateTable(array){
+    let newRow = document.getElementById("data").insertRow(-1);
+    for (let item of array) {
+        let thing = newRow.insertCell();
+        thing.innerHTML = item;
+    }
 }
 
 function mean(){
@@ -37,17 +36,19 @@ function mean(){
     for (let number of result) {
         sum += number;
     }
-    return(sum / result.length);
+
+    return(Math.round((sum / result.length) * 100) / 100);
 }
 
 function median(){
-    result.sort();
-    if (result.length % 2 == 0){
-        let num1 = result[result.length/2];
-        let num2 = result[(result.length/2) - 1];
-        return (num1 + num2) / 2
+    let arr = result;
+    arr.sort();
+    if (arr.length % 2 == 0){
+        let num1 = arr[arr.length/2];
+        let num2 = arr[(arr.length/2) - 1];
+        return (num1 + num2) / 2;
     }else{
-        return result[Math.floor(result.length/2)];
+        return arr[Math.floor(arr.length/2)];
     }
 }
 
@@ -55,7 +56,7 @@ function mode () {
     let numMode = 0;
     let numOfMode = 0;
     for (i = 0; i < result.length; i ++){
-        let arr = result.filter(modeHelper(result[i]));
+        let arr = result.filter(modeHelper, numMode);
         if (i === 0 || arr.length > numOfMode) {
             numMode = result[i];
             numOfMode = arr.length; 
@@ -71,36 +72,27 @@ function modeHelper(num, mode){
 function frequency() {
     let list = [];
     for (i = 0; i < numDice * 6; i ++) {
-        list.push([numDice + i]);
+        list.push(numDice + i);
     }
     for (let num of removeDups(result)){
         let count = 0;
         for (i = 0; i < result.length; i ++) {
-            if (result[i] === num) {
+            if (result[i] == num) {
                 count ++;
             }
         }
         for (i = 0; i < list.length; i ++) {
-            if (list[i][0] === num) {
-                list[i].push(count);
-            }
-        }
-    }
-    if (numDice >= 2) {
-        for (let cs of list) {
-            if (cs[1] >= 2){
-                freq2 ++;
-                if (cs[1] === 3) {
-                    freq3 ++;
-                }
-            }
+            if (list[i] == num) {
+                list[i] = [list[i], count];
+            }   
         }
     }
     return list;
 }
 
 function removeDups(array) {
-    return array.sort().filter(function(item, position, arr) {
+    let arre = array;
+    return arre.sort().filter(function(item, position, arr) {
         return !position || item != arr[position - 1];
     });
 }
